8b680a7be1b74acd26e7427c69ca93d99eebf94f
ValeryWelker <127634102+ValeryWelker@users.noreply.github.com>
2024-01-07 22:41:15 +0300

    Add files via upload

--- geolocation.js
+++ geolocation.js
@@ -53,6 +53,33 @@ navigator.geolocation.getCurrentPosition(function(position) {
     // Добавляем метку на карту
     map.geoObjects.add(placemark);
     
+
+        // Создаем экземпляр класса ymaps.control.SearchControl
+        mySearchControl = new ymaps.control.SearchControl({
+          options: {
+              noPlacemark: true
+          }
+      }),
+  // Результаты поиска будем помещать в коллекцию.
+      mySearchResults = new ymaps.GeoObjectCollection(null, {
+          hintContentLayout: ymaps.templateLayoutFactory.createClass('$[properties.name]')
+      });
+  myMap.controls.add(mySearchControl);
+  myMap.geoObjects.add(mySearchResults);
+  // При клике по найденному объекту метка становится красной.
+  mySearchResults.events.add('click', function (e) {
+      e.get('target').options.set('preset', 'islands#redIcon');
+  });
+  // Выбранный результат помещаем в коллекцию.
+  mySearchControl.events.add('resultselect', function (e) {
+      var index = e.get('index');
+      mySearchControl.getResult(index).then(function (res) {
+         mySearchResults.add(res);
+      });
+  }).add('submit', function () {
+          mySearchResults.removeAll();
+      })
+    
     document.getElementById('destroyButton').onclick = function () {
       // Для уничтожения используется метод destroy.
       myMap.destroy()
